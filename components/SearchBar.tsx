4e39462280b5ad3e9aeaa5fd06c276e389aac953
muneeb3123 <rehmanmuneeb66@gmail.com>
2024-03-31 23:54:21 +0500

    add paragaraph

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,7 +1,10 @@
+import { Combobox } from "@headlessui/react"
+
 const SearchBar = () => {
   return (
     <div>
      <Combobox>Hello</Combobox>
+     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus similique praesentium error nulla est. Commodi, itaque ad minima iste incidunt, pariatur fugit, sed quae sapiente fuga illo iure expedita unde?</p>
     </div>
   )
 }
