2013b597ef16ab3d42974e142f7268b812e00a65
muneeb3123 <rehmanmuneeb66@gmail.com>
2024-04-01 23:17:09 +0500

    add search heading

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -3,6 +3,7 @@ import { Combobox } from "@headlessui/react"
 const SearchBar = () => {
   return (
     <div>
+      <h1>Search Bar</h1>
      <Combobox>Hello</Combobox>
      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus similique praesentium error nulla est. Commodi, itaque ad minima iste incidunt, pariatur fugit, sed quae sapiente fuga illo iure expedita unde?</p>
     </div>
