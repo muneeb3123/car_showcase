9ce5d63ad04a91114fa61f084e047456366656af
muneeb3123 <rehmanmuneeb66@gmail.com>
2024-04-03 18:19:44 +0500

    add combobox

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,21 @@
-import { Daud, Hero, SearchBar} from "@/components";
+import { CustomFilter,  Hero, SearchBar} from "@/components";
 
 export default function Home() {
   return (
     <main className="overflow-hidden">
-      <Daud />
-      <div className="padding-x padding-y mt-12">
+      <Hero />
+      <div className="padding-x padding-y mt-12 max-width"  id="discover">
+        <div className="home__text-container">
+          <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
+          <p>Explore the cars you might like</p>
+        </div>
+        <div className="home__filters">
         <SearchBar />
+        <div className="home__filter-container">
+          <CustomFilter title ="fuel" />
+          <CustomFilter title ="year" />
+        </div>
+        </div>
       </div>
     </main>
   );
