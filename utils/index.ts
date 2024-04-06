8364a6b702d4270ecdd183e40989df7fb3189a36
muneeb3123 <rehmanmuneeb66@gmail.com>
2024-04-06 22:20:27 +0500

    fetch cars from api

--- utils/index.ts
+++ utils/index.ts
@@ -55,28 +55,28 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
 //   return newPathname;
 // };
 
-// export async function fetchCars(filters: FilterProps) {
-//   const { manufacturer, year, model, limit, fuel } = filters;
+export async function fetchCars(filters: FilterProps) {
+  const { manufacturer, year, model, limit, fuel } = filters;
 
-//   // Set the required headers for the API request
-//   const headers: HeadersInit = {
-//     "X-RapidAPI-Key": process.env.NEXT_PUBLIC_RAPID_API_KEY || "",
-//     "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
-//   };
+  // Set the required headers for the API request
+  const headers: HeadersInit = {
+    "X-RapidAPI-Key": process.env.NEXT_PUBLIC_RAPID_API_KEY || "",
+    "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
+  };
 
-//   // Set the required headers for the API request
-//   const response = await fetch(
-//     `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,
-//     {
-//       headers: headers,
-//     }
-//   );
+  // Set the required headers for the API request
+  const response = await fetch(
+    `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,
+    {
+      headers: headers,
+    }
+  );
 
-//   // Parse the response as JSON
-//   const result = await response.json();
+  // Parse the response as JSON
+  const result = await response.json();
 
-//   return result;
-// }
+  return result;
+}
 
 export const generateCarImageUrl = (car: CarProps, angle?: string) => {
   const url = new URL("https://cdn.imagin.studio/getimage");
