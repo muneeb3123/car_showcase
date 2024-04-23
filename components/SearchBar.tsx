1dbf6024a1f489c83a2c2b4fbab97ff4c7444eb5
muneeb3123 <rehmanmuneeb66@gmail.com>
2024-04-23 21:30:27 +0500

    add search manufacturer

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -12,7 +12,7 @@ const SearchBar = () => {
       <div className="searchbar__item">
         <SearchManufacturer manufacturer= {manufacturer} setManufacturer={setManufacturer}/>
         <SearchManufacturer manufacturer= {manufacturer} setManufacturer={setManufacturer}/>
-
+        <SearchManufacturer manufacturer= {manufacturer} setManufacturer={setManufacturer}/>
       </div>
     </form>
   )
